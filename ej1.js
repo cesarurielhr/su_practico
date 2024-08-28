9240ed7e4253833d51bcfcbe2d334417490aa2ba
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-08-28 14:52:26 -0700

    Create eje1.js

---
+++ ej1.js
@@ -0,0 +1,9 @@
+let texto = 'Hola';
+
+
+// Contar el número de caracteres en la cadena
+let numeroDeCaracteres = texto.length;
+
+
+// Mostrar el número de caracteres en la consola
+console.log('El número de caracteres es: ' + numeroDeCaracteres);
